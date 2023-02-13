78f77a8c4fecfb77b46bb4b45a836d39d1ecb144
陈文 <chenwen@chenwendeMBP.lan>
2023-02-13 20:56:20 +0800

    ci: add eslint and commitlint

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint', 'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const calc = (a: number, b: number): void => {
+  console.log(a - b)
+}
+calc(1024, 28)
+
